e507f0275fb6fe35491ee169bd5713ddaeb75e43
varsha2050 <varshamuduli@gmail.com>
2025-05-03 04:24:36 -0400

    Add files via upload

---
+++ compound interest.py
@@ -0,0 +1,7 @@
+def_calculate_distance(x1,y1,x2,y2)
+"""
+calculate the distance between two points(x1y1) and (x2y2)
+:param x1:x-coordinate of first point(float or int)
+:param y1:y-coordinate of first point(float or int)
+:param x2:x-coordinate of second point(float or int)
+:param y2
